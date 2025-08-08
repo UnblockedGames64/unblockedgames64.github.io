4ba70af7113d4249a06a8b6d070dd3d141951791
UnblockedGames64 <jerrycantcode@gmail.com>
2025-08-08 13:38:57 -0400

    Delete unblockedgames77 directory

--- unblockedgames77/scripts/homeinject.js
+++
@@ -1,5 +0,0 @@
-const links = document.getElementsByClassName("homelink");
-
-for(let link in links) {
-    link.href = link.href = window.location.origin;
-}
--- unblockedgames77/scripts/injectgames.js
+++
@@ -1,8 +0,0 @@
-const games = [
-  { name: "Minecraft", file: "./games/minecraft.html" }
-];
-
-for(let gamei in games) {
-    let game = games[gamei];
-    document.getElementById("games-sidebar").innerHTML += `<a href='${game.file}'>${game.name}</a>`
-}
